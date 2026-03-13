579509a1b4ba4691ade353a67039d1e794548de3
adrielbibiano <adrielbibiano@gmail.com>
2026-03-13 02:36:32 +0000

    Profile

---
+++ next/app/page.js
@@ -0,0 +1,10 @@
+import Profile from "../components/Profile";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Site criado para a disciplina Programação Front-End</h1>
+      <Profile />
+    </main>
+  );
+}
---
+++ next/components/Profile.js
@@ -0,0 +1,20 @@
+export default function Profile() {
+  return (
+    <div style={{ textAlign: "center" }}>
+       <img
+        src="/eu.jpeg"
+        width="200"
+        style={{ borderRadius: "50%" }}
+      />
+
+      <h2>Adriel Bibiano</h2>
+
+      <p>
+        Estudante de Sistema para Internet 3° periodo 
+      </p>
+      <p>
+        "A misericórdia do Senhor é a causa de não sermos consumidos."
+      </p>
+    </div>
+  );
+}
